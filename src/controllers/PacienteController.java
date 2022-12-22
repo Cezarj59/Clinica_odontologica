260136785f1ef7ff3902794f4b9ca85824ffac0a
cezarj59 <cezarj59@yahoo.com>
2022-12-21 22:46:16 -0300

    create Paciente Controller

---
+++ src/controllers/PacienteController.java
@@ -0,0 +1,94 @@
+package controllers;
+
+import java.sql.*;
+import java.util.ArrayList;
+import models.Paciente;
+import services.BancoDados;
+import services.Receber;
+
+public class PacienteController {
+
+    public static Paciente cadastrar() {
+        Paciente p = new Paciente();
+        System.out.println("\nCADASTRAR DOUTOR\n");
+
+        System.out.print("Informe o Nome: ");
+        p.setNome(Receber.texto());
+
+        System.out.print("Informe o Nome: ");
+        p.setNascimento(Receber.data());
+
+        System.out.print("Informe o CPF, incluindo pontos e traço: ");
+        p.setCpf(Receber.texto());
+
+        System.out.print("Informe o Telefone: ");
+        p.setTelefone(Receber.texto());
+
+        System.out.print("Informe o Email: ");
+        p.setEmail(Receber.texto());
+        return p;
+    }
+
+    public static void addPaciente(Paciente p) {
+        Connection conn = BancoDados.conectar();
+
+        try {
+            String sql = "INSERT INTO doutor (nascimento,cpf,telefone,email,nome) VALUES (?,?,?,?,?)";
+
+            PreparedStatement statement = conn.prepareStatement(sql);
+
+            statement.setString(1, p.getNascimento().toString());
+            statement.setString(2, p.getCpf());
+            statement.setString(3, p.getTelefone());
+            statement.setString(4, p.getEmail());
+            statement.setString(5, p.getNome());
+
+            if (statement.executeUpdate() > 0) {
+                System.out.println("\n--------------------------------");
+                System.out.println("Paciente Cadastrado com Sucesso!!!");
+                System.out.println("--------------------------------\n");
+            } else {
+                System.err.println("\n------------------------------");
+                System.err.println("Falha ao Cadastrar o Paciente!!!");
+                System.err.println("------------------------------\n");
+            }
+
+        } catch (SQLException e) {
+            System.err.println(e);
+        }
+
+        BancoDados.fecha(conn);
+    }
+
+    public static ArrayList<Paciente> getAll() {
+        ArrayList<Paciente> lista = new ArrayList<>();
+        Connection conn = BancoDados.conectar();
+
+        try {
+            String sql = "SELECT * FROM doutor";
+            Statement statement = conn.createStatement();
+
+            ResultSet resultado = statement.executeQuery(sql);
+
+            while (resultado.next()) {
+                lista.add(new Paciente(
+                        resultado.getDate("nascimento").toLocalDate(),
+                        resultado.getString("cpf"),
+                        resultado.getString("telefone"),
+                        resultado.getString("email"),
+                        resultado.getInt("idPaciente"),
+                        resultado.getString("nomePaciente")
+                ));
+
+            }
+
+        } catch (SQLException e) {
+            System.out.println("ERRO AO BUSCAR: " + e);
+        }
+
+        BancoDados.fecha(conn);
+
+        return lista;
+    }
+
+}
