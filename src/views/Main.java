fd987225ce04904be148befdce23be947387ab1e
cezarj59 <cezarj59@yahoo.com>
2022-12-22 22:37:12 -0300

    teste Main

--- src/views/Main.java
+++ src/views/Main.java
@@ -1,11 +1,13 @@
-
 package views;
 
+import controllers.ConsultaAgenController;
 import controllers.DoutorController;
 
-
 public class Main {
+
     public static void main(String[] args) {
-        DoutorController.cadastrar();
+       // DoutorController.addDoutor(DoutorController.cadastrar());
+        System.out.println(ConsultaAgenController.retornaIdDoutor("Francisco", "Restauração"));
+        
     }
 }
