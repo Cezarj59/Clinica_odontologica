b4a8891bfb31baa27db64baacac363c129516bc1
cezarj59 <cezarj59@yahoo.com>
2022-12-23 19:57:33 -0300

    create controller agendamento

--- src/controllers/ConsultaAgenController.java
+++ src/controllers/ConsultaAgenController.java
@@ -12,32 +12,46 @@ import services.Receber;
 
 public class ConsultaAgenController {
 
-    public static ConsultaAgendamento agendar() {
-        ConsultaAgendamento a = new ConsultaAgendamento();
-
-        System.out.println("\nAgendar Consulta\n");
+    public static int agendarConsultaPaciente() {
 
         System.out.print("Informe o Nome do Paciente: ");
-        String nomeP = Receber.texto();
+        String nome = Receber.texto();
         System.out.print("Informe o CPF do Paciente: ");
-        String cpf = Receber.texto();
+        String cpf = Receber.cpf();
 
-        a.setIdPaciente(retornaIdPaciente(nomeP, cpf));
+        return PacienteController.retornaIdPaciente(PacienteController.buscaPaciente(nome, cpf));
+    }
+
+    public static int agendarConsultaDoutor() {
 
         System.out.print("Informe o Nome do Doutor: ");
         String nome = Receber.texto();
         System.out.print("Informe a Especialidade do Doutor: ");
         String especialidade = Receber.texto();
 
-        a.setIdDoutor(retornaIdDoutor(nome, especialidade));
+        return DoutorController.retornaIdDoutor(DoutorController.buscaDoutor(nome, especialidade));
+    }
+
+    
+
+  
+
+    public static ConsultaAgendamento agendar() {
+        ConsultaAgendamento a = new ConsultaAgendamento();
+
+        System.out.println("\nAgendar Consulta\n");
+
+        a.setIdPaciente(0);
+
+        a.setIdDoutor(0);
 
         System.out.print("Informe a data e hora da Consulta: ");
         a.setDataHoraConsulta(Receber.dataHora());
 
         System.out.print("Informe o valor da Consulta: ");
         a.setValorConsulta(Receber.numeroDecimal());
-
         return a;
+
     }
 
     public static void addConsulta(ConsultaAgendamento a) {
@@ -49,7 +63,7 @@ public class ConsultaAgenController {
             PreparedStatement statement = conn.prepareStatement(sql);
 
             statement.setInt(1, a.getIdPaciente());
-            statement.setInt(2, a.getIdPaciente());
+            statement.setInt(2, a.getIdDoutor());
             statement.setString(3, a.getDataHoraConsulta().toString());
             statement.setDouble(4, a.getValorConsulta());
 
@@ -69,68 +83,4 @@ public class ConsultaAgenController {
 
         BancoDados.fecha(conn);
     }
-
-    public static int retornaIdDoutor(String nome, String especialidade) {
-
-        Connection conn = BancoDados.conectar();
-        int id = 0;
-
-        try {
-            String sql = "SELECT id FROM doutor Where nome LIKE '%" + nome + "%' AND especialidade LIKE '%" + especialidade + "%'";
-            Statement statement = conn.createStatement();
-
-            ResultSet resultado = statement.executeQuery(sql);
-
-            while (resultado.next()) {
-                Doutor d = new Doutor(
-                        resultado.getString("cro"),
-                        resultado.getString("especialidade"),
-                        resultado.getInt("id"),
-                        resultado.getString("nome")
-                );
-
-                id = resultado.getInt("id");
-
-            }
-        } catch (SQLException e) {
-            System.out.println("ERRO AO BUSCAR: " + e);
-        }
-
-        BancoDados.fecha(conn);
-
-        return id;
-    }
-
-    public static int retornaIdPaciente(String nome, String cpf) {
-
-        int id = 0;
-        Connection conn = BancoDados.conectar();
-
-        try {
-            String sql = "SELECT * FROM paciente Where nome LIKE '%" + nome + "%' AND cpf LIKE '%" + cpf + "%'";
-            Statement statement = conn.createStatement();
-
-            ResultSet resultado = statement.executeQuery(sql);
-
-            while (resultado.next()) {
-                Paciente p = new Paciente(
-                        resultado.getDate("nascimento").toLocalDate(),
-                        resultado.getString("cpf"),
-                        resultado.getString("telefone"),
-                        resultado.getString("email"),
-                        resultado.getInt("idPaciente"),
-                        resultado.getString("nomePaciente")
-                );
-
-                id = resultado.getInt("idPaciente");
-            }
-        } catch (SQLException e) {
-            System.out.println("ERRO AO BUSCAR: " + e);
-        }
-
-        BancoDados.fecha(conn);
-
-        return id;
-    }
-
 }
