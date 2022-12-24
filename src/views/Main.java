25314ba66b35fc82e7706c2e6e805ec3c8f9b178
cezarj59 <cezarj59@yahoo.com>
2022-12-23 22:06:47 -0300

    inicio

--- src/views/Main.java
+++ src/views/Main.java
@@ -1,24 +1,11 @@
 package views;
 
-import controllers.ConsultaAgenController;
-import controllers.DoutorController;
-import controllers.PacienteController;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
-import java.util.Scanner;
-import java.util.regex.MatchResult;
-import services.Receber;
-
 public class Main {
 
     public static void main(String[] args) {
-      
-     ConsultaAgenController.addConsulta(ConsultaAgenController.agendar());
 
-      
+        Menu.inicio();
+
     }
 
 }
