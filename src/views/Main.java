9f9f2e54eec2ad52f18582ae7fc95327ed65a78c
cezarj59 <cezarj59@yahoo.com>
2022-12-26 16:59:39 -0300

    test

--- src/views/Main.java
+++ src/views/Main.java
@@ -1,14 +1,10 @@
 package views;
 
-import static controllers.ConsultaAgenController.alteraStatus;
-
 public class Main {
 
     public static void main(String[] args) {
 
-      // Menu.inicio();
-      
-      alteraStatus();
+        Menu.inicio();
 
     }
 
