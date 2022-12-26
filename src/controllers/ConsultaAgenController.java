d254105e398d52afb81d6bc01e83f10b5f0f3642
cezarj59 <cezarj59@yahoo.com>
2022-12-25 23:08:23 -0300

    finalizando os métodos de pesquisa

--- src/controllers/ConsultaAgenController.java
+++ src/controllers/ConsultaAgenController.java
@@ -1,6 +1,8 @@
 package controllers;
 
 import java.sql.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.OffsetTime;
 import java.util.ArrayList;
@@ -60,6 +62,28 @@ public class ConsultaAgenController {
         BancoDados.fecha(conn);
     }
 
+    public static void alteraStatus() {
+        ConsultaAgendamento a = new ConsultaAgendamento();
+        Connection conn = BancoDados.conectar();
+
+        try {
+
+            String sql = "SET @data = CURRENT_TIMESTAMP;"
+                    + " UPDATE consultaAgendamento SET status = 0 WHERE dataHora < @data";
+
+            PreparedStatement statement = conn.prepareStatement(sql);
+
+            statement.setBoolean(1, a.isStatusConsulta());
+            
+            statement.executeUpdate(sql);
+            
+        } catch (SQLException e) {
+            System.err.println(e);
+        }
+
+        BancoDados.fecha(conn);
+    }
+
     public static ArrayList<ConsultaAgendamento> getAll() {
         ArrayList<ConsultaAgendamento> lista = new ArrayList<>();
         Connection conn = BancoDados.conectar();
@@ -79,13 +103,253 @@ public class ConsultaAgenController {
                     + " consultaAgendamento.dataHora AS dataHora,"
                     + " consultaAgendamento.valor AS valor,"
                     + " consultaAgendamento.status AS status"
-                    + " FROM consultaAgendamento INNER JOIN paciente"
-                    + " INNER JOIN doutor ON paciente.id = doutor.id";
+                    + " FROM consultaAgendamento"
+                    + " INNER JOIN paciente ON consultaAgendamento.idPaciente = paciente.id"
+                    + " INNER JOIN doutor ON consultaAgendamento.idDoutor = doutor.id";
+
             Statement statement = conn.createStatement();
 
             ResultSet resultado = statement.executeQuery(sql);
 
             while (resultado.next()) {
+                lista.add(new ConsultaAgendamento(
+                        resultado.getDate("dataNascimento").toLocalDate(),
+                        resultado.getString("cpf"),
+                        resultado.getString("telefone"),
+                        resultado.getString("email"),
+                        resultado.getInt("idPaciente"),
+                        resultado.getString("nomePaciente"),
+                        resultado.getString("cro"),
+                        resultado.getString("especialidade"),
+                        resultado.getInt("idDoutor"),
+                        resultado.getString("nomeDoutor"),
+                        resultado.getInt("idConsulta"),
+                        resultado.getTimestamp("dataHora").toLocalDateTime(),
+                        resultado.getDouble("valor"),
+                        resultado.getBoolean("status")
+                ));
+
+            }
+
+        } catch (SQLException e) {
+            System.out.println("ERRO AO BUSCAR: " + e);
+        }
+
+        BancoDados.fecha(conn);
+
+        return lista;
+    }
+
+    public static ArrayList<ConsultaAgendamento> getHoje() {
+        ArrayList<ConsultaAgendamento> lista = new ArrayList<>();
+        Connection conn = BancoDados.conectar();
+        LocalDateTime hoje = LocalDateTime.now();
+
+        try {
+            String sql = "SELECT consultaAgendamento.id AS idConsulta,"
+                    + " paciente.id AS idPaciente,"
+                    + " paciente.nome AS nomePaciente,"
+                    + " paciente.nascimento AS dataNascimento,"
+                    + " paciente.email AS email,"
+                    + " paciente.cpf AS cpf,"
+                    + " paciente.telefone AS telefone,"
+                    + " doutor.id AS idDoutor,"
+                    + " doutor.nome AS nomeDoutor,"
+                    + " doutor.cro AS cro,"
+                    + " doutor.especialidade AS especialidade,"
+                    + " consultaAgendamento.dataHora AS dataHora,"
+                    + " consultaAgendamento.valor AS valor,"
+                    + " consultaAgendamento.status AS status"
+                    + " FROM consultaAgendamento"
+                    + " INNER JOIN paciente ON consultaAgendamento.idPaciente = paciente.id"
+                    + " INNER JOIN doutor ON consultaAgendamento.idDoutor = doutor.id";
+
+            Statement statement = conn.createStatement();
+
+            ResultSet resultado = statement.executeQuery(sql);
+
+            while (resultado.next()) {
+                LocalDateTime dataAgendada = resultado.getTimestamp("dataHora").toLocalDateTime();
+
+                if (dataAgendada.getDayOfMonth() == hoje.getDayOfMonth()) {
+                    lista.add(new ConsultaAgendamento(
+                            resultado.getDate("dataNascimento").toLocalDate(),
+                            resultado.getString("cpf"),
+                            resultado.getString("telefone"),
+                            resultado.getString("email"),
+                            resultado.getInt("idPaciente"),
+                            resultado.getString("nomePaciente"),
+                            resultado.getString("cro"),
+                            resultado.getString("especialidade"),
+                            resultado.getInt("idDoutor"),
+                            resultado.getString("nomeDoutor"),
+                            resultado.getInt("idConsulta"),
+                            resultado.getTimestamp("dataHora").toLocalDateTime(),
+                            resultado.getDouble("valor"),
+                            resultado.getBoolean("status")
+                    ));
+                }
+
+            }
+
+        } catch (SQLException e) {
+            System.out.println("ERRO AO BUSCAR: " + e);
+        }
+
+        BancoDados.fecha(conn);
+
+        return lista;
+    }
+
+    public static ArrayList<ConsultaAgendamento> getEspecialidade(String especialidadeParametro) {
+        ArrayList<ConsultaAgendamento> lista = new ArrayList<>();
+        Connection conn = BancoDados.conectar();
+        LocalDateTime hoje = LocalDateTime.now();
+
+        try {
+            String sql = "SELECT consultaAgendamento.id AS idConsulta,"
+                    + " paciente.id AS idPaciente,"
+                    + " paciente.nome AS nomePaciente,"
+                    + " paciente.nascimento AS dataNascimento,"
+                    + " paciente.email AS email,"
+                    + " paciente.cpf AS cpf,"
+                    + " paciente.telefone AS telefone,"
+                    + " doutor.id AS idDoutor,"
+                    + " doutor.nome AS nomeDoutor,"
+                    + " doutor.cro AS cro,"
+                    + " doutor.especialidade AS especialidade,"
+                    + " consultaAgendamento.dataHora AS dataHora,"
+                    + " consultaAgendamento.valor AS valor,"
+                    + " consultaAgendamento.status AS status"
+                    + " FROM consultaAgendamento"
+                    + " INNER JOIN paciente ON consultaAgendamento.idPaciente = paciente.id"
+                    + " INNER JOIN doutor ON consultaAgendamento.idDoutor = doutor.id"
+                    + " WHERE doutor.especialidade LIKE '%" + especialidadeParametro + "%'";
+
+            Statement statement = conn.createStatement();
+
+            ResultSet resultado = statement.executeQuery(sql);
+
+            while (resultado.next()) {
+
+                lista.add(new ConsultaAgendamento(
+                        resultado.getDate("dataNascimento").toLocalDate(),
+                        resultado.getString("cpf"),
+                        resultado.getString("telefone"),
+                        resultado.getString("email"),
+                        resultado.getInt("idPaciente"),
+                        resultado.getString("nomePaciente"),
+                        resultado.getString("cro"),
+                        resultado.getString("especialidade"),
+                        resultado.getInt("idDoutor"),
+                        resultado.getString("nomeDoutor"),
+                        resultado.getInt("idConsulta"),
+                        resultado.getTimestamp("dataHora").toLocalDateTime(),
+                        resultado.getDouble("valor"),
+                        resultado.getBoolean("status")
+                ));
+
+            }
+
+        } catch (SQLException e) {
+            System.out.println("ERRO AO BUSCAR: " + e);
+        }
+
+        BancoDados.fecha(conn);
+
+        return lista;
+    }
+
+    public static ArrayList<ConsultaAgendamento> getPaciente(String nomePacienteParametro) {
+        ArrayList<ConsultaAgendamento> lista = new ArrayList<>();
+        Connection conn = BancoDados.conectar();
+        LocalDateTime hoje = LocalDateTime.now();
+
+        try {
+            String sql = "SELECT consultaAgendamento.id AS idConsulta,"
+                    + " paciente.id AS idPaciente,"
+                    + " paciente.nome AS nomePaciente,"
+                    + " paciente.nascimento AS dataNascimento,"
+                    + " paciente.email AS email,"
+                    + " paciente.cpf AS cpf,"
+                    + " paciente.telefone AS telefone,"
+                    + " doutor.id AS idDoutor,"
+                    + " doutor.nome AS nomeDoutor,"
+                    + " doutor.cro AS cro,"
+                    + " doutor.especialidade AS especialidade,"
+                    + " consultaAgendamento.dataHora AS dataHora,"
+                    + " consultaAgendamento.valor AS valor,"
+                    + " consultaAgendamento.status AS status"
+                    + " FROM consultaAgendamento"
+                    + " INNER JOIN paciente ON consultaAgendamento.idPaciente = paciente.id"
+                    + " INNER JOIN doutor ON consultaAgendamento.idDoutor = doutor.id"
+                    + " WHERE paciente.nome LIKE '%" + nomePacienteParametro + "%'";
+
+            Statement statement = conn.createStatement();
+
+            ResultSet resultado = statement.executeQuery(sql);
+
+            while (resultado.next()) {
+
+                lista.add(new ConsultaAgendamento(
+                        resultado.getDate("dataNascimento").toLocalDate(),
+                        resultado.getString("cpf"),
+                        resultado.getString("telefone"),
+                        resultado.getString("email"),
+                        resultado.getInt("idPaciente"),
+                        resultado.getString("nomePaciente"),
+                        resultado.getString("cro"),
+                        resultado.getString("especialidade"),
+                        resultado.getInt("idDoutor"),
+                        resultado.getString("nomeDoutor"),
+                        resultado.getInt("idConsulta"),
+                        resultado.getTimestamp("dataHora").toLocalDateTime(),
+                        resultado.getDouble("valor"),
+                        resultado.getBoolean("status")
+                ));
+
+            }
+
+        } catch (SQLException e) {
+            System.out.println("ERRO AO BUSCAR: " + e);
+        }
+
+        BancoDados.fecha(conn);
+
+        return lista;
+    }
+
+    public static ArrayList<ConsultaAgendamento> getDoutor(String nomeDoutorParametro) {
+        ArrayList<ConsultaAgendamento> lista = new ArrayList<>();
+        Connection conn = BancoDados.conectar();
+        LocalDateTime hoje = LocalDateTime.now();
+
+        try {
+            String sql = "SELECT consultaAgendamento.id AS idConsulta,"
+                    + " paciente.id AS idPaciente,"
+                    + " paciente.nome AS nomePaciente,"
+                    + " paciente.nascimento AS dataNascimento,"
+                    + " paciente.email AS email,"
+                    + " paciente.cpf AS cpf,"
+                    + " paciente.telefone AS telefone,"
+                    + " doutor.id AS idDoutor,"
+                    + " doutor.nome AS nomeDoutor,"
+                    + " doutor.cro AS cro,"
+                    + " doutor.especialidade AS especialidade,"
+                    + " consultaAgendamento.dataHora AS dataHora,"
+                    + " consultaAgendamento.valor AS valor,"
+                    + " consultaAgendamento.status AS status"
+                    + " FROM consultaAgendamento"
+                    + " INNER JOIN paciente ON consultaAgendamento.idPaciente = paciente.id"
+                    + " INNER JOIN doutor ON consultaAgendamento.idDoutor = doutor.id"
+                    + " WHERE doutor.nome LIKE '%" + nomeDoutorParametro + "%'";
+
+            Statement statement = conn.createStatement();
+
+            ResultSet resultado = statement.executeQuery(sql);
+
+            while (resultado.next()) {
+
                 lista.add(new ConsultaAgendamento(
                         resultado.getDate("dataNascimento").toLocalDate(),
                         resultado.getString("cpf"),
