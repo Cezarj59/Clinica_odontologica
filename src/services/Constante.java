413c703146dc48fdbf22d43a0d5ecaae1196d322
cezarj59 <cezarj59@yahoo.com>
2023-01-20 21:34:07 -0300

    feat - Criando um Classe com variaveis constantes

---
+++ src/services/Constante.java
@@ -0,0 +1,12 @@
+package services;
+
+import controllers.AgendaConsultaController;
+import controllers.DoutorController;
+import controllers.PacienteController;
+
+public class Constante {
+
+   public static PacienteController paciente = new PacienteController();
+   public static AgendaConsultaController agendamento = new AgendaConsultaController();
+   public static DoutorController doutor = new DoutorController();
+}
