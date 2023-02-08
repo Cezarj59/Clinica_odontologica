cdd44f4671ca54087a783492c2eab75217945386
cezarj59 <cezarj59@yahoo.com>
2023-02-07 23:06:27 -0300

    feature - iniciando a aplicação e  look and feel

--- src/views/Main.java
+++ src/views/Main.java
@@ -1,10 +1,16 @@
 package views;
 
+import java.awt.Color;
+import javax.swing.UIManager;
+import services.AplicaNimbusLookAndFeel;
+
 public class Main {
 
     public static void main(String[] args) {
-
-        Menu.autenticar();
+        AplicaNimbusLookAndFeel.pegaNimbus();
+        UIManager.put("nimbusBase", new Color(51, 153, 255));
+        InterfacePrincipal i = new InterfacePrincipal();
+        i.setVisible(true);
 
     }
 
