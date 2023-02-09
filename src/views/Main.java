61ff66100f2e7d403cd71fc25b9077b49bb651fe
cezarj59 <cezarj59@yahoo.com>
2023-02-08 23:42:32 -0300

    feature - chamando a interface

--- src/views/Main.java
+++ src/views/Main.java
@@ -1,14 +1,10 @@
 package views;
 
-import java.awt.Color;
-import javax.swing.UIManager;
-import services.AplicaNimbusLookAndFeel;
-
 public class Main {
 
     public static void main(String[] args) {
-        AplicaNimbusLookAndFeel.pegaNimbus();
-        UIManager.put("nimbusBase", new Color(51, 153, 255));
+        LookAndFell style = new LookAndFell();
+        style.Nimbus();
         InterfacePrincipal i = new InterfacePrincipal();
         i.setVisible(true);
 
