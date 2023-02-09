799509c70b87d403f95b9ed35b11c9b5cf97d6c5
cezarj59 <cezarj59@yahoo.com>
2023-02-08 23:42:50 -0300

    feature - criando as views

--- src/views/Views.java
+++ src/views/Views.java
@@ -4,209 +4,298 @@ import models.Doutor;
 import models.Paciente;
 import models.ConsultaAgendamento;
 import services.Constante;
-import controllers.*;
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 import services.Receber;
 
 public class Views {
 
-    public static void consultaPacienteTodos() {
+    public static void consultaPacienteTodos(JTable tabela) {
         ArrayList<Paciente> pacientes = Constante.paciente.getAll();
-        System.out.println("\n----Consulta de Pacientes Todos----");
+
         if (pacientes.isEmpty()) {
-            System.out.println("\nNão há Pacientes Cadastrados!!!\n");
+            JOptionPane.showMessageDialog(null, "\nNão há Pacientes Cadastrados!!!\n");
         } else {
+            DefaultTableModel modelo = (DefaultTableModel) tabela.getModel();
+
             for (Paciente p : pacientes) {
-                System.out.println(p.toString());
+                modelo.addRow(new Object[]{
+                    p.getId(),
+                    p.getNome(),
+                    p.getNascimento(),
+                    p.getCpf(),
+                    p.getTelefone(),
+                    p.getEmail()});
 
             }
         }
 
     }
 
-    public static void consultaPacienteNome() {
-        System.out.print("Informe o Nome: ");
-        String nomePaciente = "";
+    public static void consultaPacienteNome(String nomePaciente, JTable tabela) {
 
         ArrayList<Paciente> pacientesPorNome = Constante.paciente.getNome(nomePaciente);
 
-        System.out.println("\n----Consulta de Pacientes por Nome----");
         if (pacientesPorNome.isEmpty()) {
-            System.out.println("\nNão há Pacientes cadastrados com o nome: " + nomePaciente + "\n");
+            JOptionPane.showMessageDialog(null, "\nNão há Pacientes cadastrados com o nome: " + nomePaciente + "\n");
         } else {
+            DefaultTableModel modelo = (DefaultTableModel) tabela.getModel();
+
             for (Paciente p : pacientesPorNome) {
-                System.out.println(p.toString());
+                modelo.addRow(new Object[]{
+                    p.getId(),
+                    p.getNome(),
+                    p.getNascimento(),
+                    p.getCpf(),
+                    p.getTelefone(),
+                    p.getEmail()});
+
             }
         }
 
     }
 
-    /*public static void consultaPacienteCpf() {
-        System.out.print("Informe o CPF: ");
-        String cpfPaciente = Receber.cpf();
+    public static void consultaPacienteCpf(String cpfPaciente, JTable tabela) {
 
-        ArrayList<Paciente> pacientesPorCpf = Constante.paciente.getCpf(cpfPaciente);
+        ArrayList<Paciente> pacientesPorCpf = Constante.paciente.getCpf(Receber.cpf(cpfPaciente));
 
-        System.out.println("\n----Consulta de Pacientes por CPF----");
         if (pacientesPorCpf.isEmpty()) {
-            System.out.println("\nNão há Pacientes cadastrados com o CPF: " + cpfPaciente + "\n");
+            JOptionPane.showMessageDialog(null, "\nNão há Pacientes cadastrados com o CPF: " + cpfPaciente);
         } else {
+
+            DefaultTableModel modelo = (DefaultTableModel) tabela.getModel();
+
             for (Paciente p : pacientesPorCpf) {
-                System.out.println(p.toString());
+                modelo.addRow(new Object[]{
+                    p.getId(),
+                    p.getNome(),
+                    p.getNascimento(),
+                    p.getCpf(),
+                    p.getTelefone(),
+                    p.getEmail()});
+
             }
+
         }
 
     }
-*/
-    public static void consultaPacienteAniversariante() {
+
+    public static void consultaPacienteAniversariante(JTable tabela) {
 
         ArrayList<Paciente> pacientesAniversariantesDoMes = Constante.paciente.getAniversariantes();
-        System.out.println("\n----Consulta de Pacientes Aniversariantes do Mês----");
+
         if (pacientesAniversariantesDoMes.isEmpty()) {
-            System.out.println("\nNão há Pacientes aniversariantes este Mês\n");
+            JOptionPane.showMessageDialog(null, "\nNão há Pacientes aniversariantes este Mês\n");
         } else {
+            DefaultTableModel modelo = (DefaultTableModel) tabela.getModel();
+
             for (Paciente p : pacientesAniversariantesDoMes) {
-                System.out.println(p.toString());
+                modelo.addRow(new Object[]{
+                    p.getId(),
+                    p.getNome(),
+                    p.getNascimento(),
+                    p.getCpf(),
+                    p.getTelefone(),
+                    p.getEmail()});
+
             }
         }
 
     }
 
-    public static void consultaDoutoresTodos() {
+    public static void consultaDoutoresTodos(JTable tabela) {
         ArrayList<Doutor> doutores = Constante.doutor.getAll();
-        System.out.println("\n-------Consulta de Doutores Todos-------");
+
         if (doutores.isEmpty()) {
-            System.out.println("\nNão há Doutores Cadastrados!!!\n");
+            JOptionPane.showMessageDialog(null, "\nNão há Doutores Cadastrados!!!\n");
         } else {
+            DefaultTableModel modelo = (DefaultTableModel) tabela.getModel();
+
             for (Doutor d : doutores) {
-                System.out.println(d.toString());
+                modelo.addRow(new Object[]{
+                    d.getId(),
+                    d.getNome(),
+                    d.getEspecialidade(),
+                    d.getCro()
+                });
 
             }
         }
 
     }
 
-    public static void consultaDoutorNome() {
-        System.out.print("Informe o Nome: ");
-        String nomeDoutor = "";
-        
+    public static void consultaDoutorNome(String nomeDoutor, JTable tabela) {
+
         ArrayList<Doutor> doutoresNome = Constante.doutor.getNome(nomeDoutor);
-        System.out.println("\n----Consulta de Doutores por Nome----");
+
         if (doutoresNome.isEmpty()) {
-            System.out.println("\nNão há Doutores cadastrados com o nome: " + nomeDoutor + "\n");
+            JOptionPane.showMessageDialog(null, "\nNão há Doutores cadastrados com o nome: " + nomeDoutor + "\n");
         } else {
+            DefaultTableModel modelo = (DefaultTableModel) tabela.getModel();
+
             for (Doutor d : doutoresNome) {
-                System.out.println(d.toString());
+                modelo.addRow(new Object[]{
+                    d.getId(),
+                    d.getNome(),
+                    d.getEspecialidade(),
+                    d.getCro()});
 
             }
         }
 
     }
 
-    public static void consultaDoutorEspecialidade() {
-        System.out.print("Informe a Especialidade: ");
-        String especialidade = "";
-        
-        ArrayList<Doutor> doutoresPorEspec = Constante.doutor.getEspecialidade(especialidade);
-        System.out.println("\n----Consulta de Doutores por Especialidade----");
-        if (doutoresPorEspec.isEmpty()) {
-            System.out.println("\nNão há Doutores cadastrados com a Especialidade: " + especialidade + "\n");
+    public static void consultaDoutorEspecialidade(String especialidade, JTable tabela) {
+
+        ArrayList<Doutor> doutoresEspecialidades = Constante.doutor.getEspecialidade(especialidade);
+
+        if (doutoresEspecialidades.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "\nNão há Doutores cadastrados com a Especialidade: " + especialidade + "\n");
         } else {
-            for (Doutor d : doutoresPorEspec) {
-                System.out.println(d.toString());
+            DefaultTableModel modelo = (DefaultTableModel) tabela.getModel();
+
+            for (Doutor d : doutoresEspecialidades) {
+                modelo.addRow(new Object[]{
+                    d.getId(),
+                    d.getNome(),
+                    d.getEspecialidade(),
+                    d.getCro()});
 
             }
         }
 
     }
 
-    public static void consultaAgendadaTodos() {
+    public static void consultaAgendadaTodos(JTable tabela) {
         ArrayList<ConsultaAgendamento> consultas = Constante.agendamento.getAll();
-        System.out.println("\n----------Consulta-----------");
+
         if (consultas.isEmpty()) {
-            System.out.println("\nNão há Consultas Agendadas!!!\n");
+            JOptionPane.showMessageDialog(null, "\nNão há Consultas Agendadas!!!\n");
         } else {
-            for (ConsultaAgendamento c : consultas) {
-                System.out.println(c.toString());
+            DefaultTableModel modelo = (DefaultTableModel) tabela.getModel();
+            for (ConsultaAgendamento p : consultas) {
+                modelo.addRow(new Object[]{
+                    p.getId(),
+                    p.getPaciente().getNome(),
+                    p.getDoutor().getNome(),
+                    p.getDoutor().getEspecialidade(),
+                    p.getDataHoraConsultaFormated(),
+                    p.getValorConsulta(),
+                    p.isStatusConsulta() ? "Agendada" : "Expirada"});
 
             }
         }
 
     }
 
-    public static void consultaAgendadaAtivas() {
-        ArrayList<ConsultaAgendamento> consultas = Constante.agendamento.getAtivo();
-        System.out.println("\n----------Consultas Ativas-----------");
-        if (consultas.isEmpty()) {
-            System.out.println("\nNão há Consultas Agendadas!!!\n");
+    public static void consultaAgendadaAtivas(JTable tabela) {
+        ArrayList<ConsultaAgendamento> consultasAtivas = Constante.agendamento.getAtivo();
+        if (consultasAtivas.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "\nNão há Consultas Agendadas!!!\n");
         } else {
-            for (ConsultaAgendamento c : consultas) {
-                System.out.println(c.toString());
+            DefaultTableModel modelo = (DefaultTableModel) tabela.getModel();
+            for (ConsultaAgendamento p : consultasAtivas) {
+                modelo.addRow(new Object[]{
+                    p.getId(),
+                    p.getPaciente().getNome(),
+                    p.getDoutor().getNome(),
+                    p.getDoutor().getEspecialidade(),
+                    p.getDataHoraConsultaFormated(),
+                    p.getValorConsulta(),
+                    p.isStatusConsulta() ? "Agendada" : "Expirada"});
 
             }
         }
 
     }
 
-    public static void consultaAgendadaHoje() {
+    public static void consultaAgendadaHoje(JTable tabela) {
         ArrayList<ConsultaAgendamento> consultasHoje = Constante.agendamento.getHoje();
-        System.out.println("\n----------Consulta(s) Agendadas para Hoje-----------");
+
         if (consultasHoje.isEmpty()) {
-            System.out.println("\nNão há consultas agendadas para Hoje!!!\n");
+            JOptionPane.showMessageDialog(null, "\nNão há consultas agendadas para Hoje!!!\n");
         } else {
-            for (ConsultaAgendamento c : consultasHoje) {
-                System.out.println(c.toString());
+            DefaultTableModel modelo = (DefaultTableModel) tabela.getModel();
+            for (ConsultaAgendamento p : consultasHoje) {
+                modelo.addRow(new Object[]{
+                    p.getId(),
+                    p.getPaciente().getNome(),
+                    p.getDoutor().getNome(),
+                    p.getDoutor().getEspecialidade(),
+                    p.getDataHoraConsultaFormated(),
+                    p.getValorConsulta(),
+                    p.isStatusConsulta() ? "Agendada" : "Expirada"});
 
             }
         }
 
     }
 
-    public static void consultaAgendadaEspecialidade() {
-        System.out.print("Informe a Especialidade: ");
-        String especialidade = "";
-        
-        ArrayList<ConsultaAgendamento> consultasPorEsp = Constante.agendamento.getEspecialidade(especialidade);
-        System.out.println("\n----------Consulta(s) Por Especialidade -----------");
-        if (consultasPorEsp.isEmpty()) {
-            System.out.println("\nNão há consultas agendadas com está Especialidade!!!\n");
+    public static void consultaAgendadaEspecialidade(String especialidade, JTable tabela) {
+
+        ArrayList<ConsultaAgendamento> consultasEspecialidade = Constante.agendamento.getEspecialidade(especialidade);
+
+        if (consultasEspecialidade.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "\nNão há consultas agendadas com está Especialidade!!!\n");
         } else {
-            for (ConsultaAgendamento c : consultasPorEsp) {
-                System.out.println(c.toString());
+            DefaultTableModel modelo = (DefaultTableModel) tabela.getModel();
+            for (ConsultaAgendamento p : consultasEspecialidade) {
+                modelo.addRow(new Object[]{
+                    p.getId(),
+                    p.getPaciente().getNome(),
+                    p.getDoutor().getNome(),
+                    p.getDoutor().getEspecialidade(),
+                    p.getDataHoraConsultaFormated(),
+                    p.getValorConsulta(),
+                    p.isStatusConsulta() ? "Agendada" : "Expirada"});
 
             }
         }
 
     }
 
-    public static void consultaAgendadaNomePaciente() {
-        System.out.print("Informe o nome do Paciente: ");
-        String nomePaciente = "";
-        
-        ArrayList<ConsultaAgendamento> consultasPorPaciente = Constante.agendamento.getPaciente(nomePaciente);
-        System.out.println("\n----------Consulta(s) Por Paciente-----------");
+    public static void consultaAgendadaNomePaciente(String nome, JTable tabela) {
+
+        ArrayList<ConsultaAgendamento> consultasPorPaciente = Constante.agendamento.getPaciente(nome);
+
         if (consultasPorPaciente.isEmpty()) {
-            System.out.println("\nNão há consultas agendadas com este Paciente!!!\n");
+            JOptionPane.showMessageDialog(null, "\nNão há consultas agendadas com este Paciente!!!\n");
         } else {
-            for (ConsultaAgendamento c : consultasPorPaciente) {
-                System.out.println(c.toString());
+            DefaultTableModel modelo = (DefaultTableModel) tabela.getModel();
+            for (ConsultaAgendamento p : consultasPorPaciente) {
+                modelo.addRow(new Object[]{
+                    p.getId(),
+                    p.getPaciente().getNome(),
+                    p.getDoutor().getNome(),
+                    p.getDoutor().getEspecialidade(),
+                    p.getDataHoraConsultaFormated(),
+                    p.getValorConsulta(),
+                    p.isStatusConsulta() ? "Agendada" : "Expirada"});
 
             }
         }
 
     }
 
-    public static void consultaAgendadaNomeDoutor() {
-        System.out.print("Informe o nome do Doutor: ");
-        String nomeDoutor = "";
-        
-        ArrayList<ConsultaAgendamento> consultasPorDoutor = Constante.agendamento.getDoutor(nomeDoutor);
-        System.out.println("\n----------Consulta(s) Por Doutor-----------");
+    public static void consultaAgendadaNomeDoutor(String nome, JTable tabela) {
+
+        ArrayList<ConsultaAgendamento> consultasPorDoutor = Constante.agendamento.getDoutor(nome);
+
         if (consultasPorDoutor.isEmpty()) {
-            System.out.println("\nNão há consultas agendadas com este Doutor!!!\n");
+            JOptionPane.showMessageDialog(null, "\nNão há consultas agendadas com este Doutor!!!\n");
         } else {
-            for (var c : consultasPorDoutor) {
-                System.out.println(c.toString());
+            DefaultTableModel modelo = (DefaultTableModel) tabela.getModel();
+            for (ConsultaAgendamento p : consultasPorDoutor) {
+                modelo.addRow(new Object[]{
+                    p.getId(),
+                    p.getPaciente().getNome(),
+                    p.getDoutor().getNome(),
+                    p.getDoutor().getEspecialidade(),
+                    p.getDataHoraConsultaFormated(),
+                    p.getValorConsulta(),
+                    p.isStatusConsulta() ? "Agendada" : "Expirada"});
 
             }
         }
