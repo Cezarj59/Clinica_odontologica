1675eb0fd685de4105c647ee8fd5c1779413aa11
cezarj59 <cezarj59@yahoo.com>
2023-02-09 22:48:53 -0300

    feature - implementando método logof no menu

--- src/views/interfacesAgendamento/InterfaceAgendamento.java
+++ src/views/interfacesAgendamento/InterfaceAgendamento.java
@@ -17,6 +17,7 @@ import javax.swing.border.BevelBorder;
 import models.ConsultaAgendamento;
 import services.Receber;
 import views.InterfacePrincipal;
+import static views.autenticacao.Autenticador.disconect;
 import views.interfacesConsultasAgendadas.*;
 import views.interfaceConsultaDoutor.*;
 import views.interfacesAgendamento.*;
@@ -93,6 +94,8 @@ public class InterfaceAgendamento extends javax.swing.JFrame {
         menuConsultasAgendadasPorPaciente = new javax.swing.JMenuItem();
         menuConsultaAgendadaPorDoutor = new javax.swing.JMenuItem();
         menuConsultaAgendadaPorEspecialidade = new javax.swing.JMenuItem();
+        jSeparator4 = new javax.swing.JPopupMenu.Separator();
+        logof = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(0, 153, 153));
@@ -540,6 +543,15 @@ public class InterfaceAgendamento extends javax.swing.JFrame {
             }
         });
         inicio.add(menuConsultaAgendadaPorEspecialidade);
+        inicio.add(jSeparator4);
+
+        logof.setText("logof");
+        logof.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                logofActionPerformed(evt);
+            }
+        });
+        inicio.add(logof);
 
         jMenuBar2.add(inicio);
 
@@ -753,6 +765,13 @@ public class InterfaceAgendamento extends javax.swing.JFrame {
         i.setVisible(true);
     }//GEN-LAST:event_menuConsultaAgendadaPorEspecialidadeActionPerformed
 
+    private void logofActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_logofActionPerformed
+        // TODO add your handling code here:
+        this.dispose();
+        disconect();
+
+    }//GEN-LAST:event_logofActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -829,6 +848,8 @@ public class InterfaceAgendamento extends javax.swing.JFrame {
     private javax.swing.JPopupMenu.Separator jSeparator1;
     private javax.swing.JPopupMenu.Separator jSeparator2;
     private javax.swing.JPopupMenu.Separator jSeparator3;
+    private javax.swing.JPopupMenu.Separator jSeparator4;
+    private javax.swing.JMenuItem logof;
     private javax.swing.JMenuItem menuAgendamentoConsulta;
     private javax.swing.JMenuItem menuCadastraDoutor;
     private javax.swing.JMenuItem menuCadastraPaciente;
