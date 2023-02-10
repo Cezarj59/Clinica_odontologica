d4946dd86f8ef256bfa40a8851f00b3b45405112
cezarj59 <cezarj59@yahoo.com>
2023-02-09 22:46:48 -0300

    feature - implementando chamada da tela de Login

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+import views.InterfaceLogin;
+import views.LookAndFell;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        LookAndFell style = new LookAndFell();
+        style.Nimbus();
+
+        InterfaceLogin login = new InterfaceLogin();
+        login.setVisible(true);
+
+    }
+
+}
